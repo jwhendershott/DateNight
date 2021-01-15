7ff3e0b88553133d77afd726be9dbe0fa3571715
Jacob Hendershott <jwhendershott@gmail.com>
2021-01-15 12:39:30 -0600

    update

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 'use strict';
 
 //set variables
-var city = ''
-var breweryURL = "https://api.openbrewerydb.org/breweries?by_city=" + city + "&sort=-name ";
+var city = document.getElementById("city-field");
+const breweryURL = "https://api.openbrewerydb.org/breweries?by_city=" + city + "&sort=-name ";
+const tmKey = "8IbZBo40AnRzs987iA70DEBWvamJn6Ub"
 
 //generates call and logs response for openbrewery
 $.ajax({
@@ -10,4 +11,30 @@ $.ajax({
     method: "GET"
 }).then(function(response) {
     console.log(response);
+});
+
+//Ticketmast call
+$.ajax({
+    type:"GET",
+    url:"https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=" + tmKey + "&city=" + city,
+    async:true,
+    dataType: "json",
+    success: function(json) {
+                console.log(json);
+                // Parse the response.
+                // Do other things.
+             },
+    error: function(err) {
+                // This time, we do not end up here!
+                console.log(err)
+             }
+  });
+
+// .on("click") function associated with Find Drinks button
+$("#find-drinks").on("click", function(event) {
+    event.preventDefault();
+    city = document.getElementById("city-field").value;
+
+    //logs updated city value to console
+    console.log(city);
 });
