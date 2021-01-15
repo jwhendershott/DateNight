448da24af2c46c54f8767f4451de8562fa126f18
jwhendershott <70990105+jwhendershott@users.noreply.github.com>
2021-01-15 12:01:11 -0600

    Revert "updated call for TM API and created function to update city value on …"

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 'use strict';
 
 //set variables
-var city = document.getElementById("city-field");
-const breweryURL = "https://api.openbrewerydb.org/breweries?by_city=" + city + "&sort=-name ";
-const tmKey = "8IbZBo40AnRzs987iA70DEBWvamJn6Ub"
+var city = ''
+var breweryURL = "https://api.openbrewerydb.org/breweries?by_city=" + city + "&sort=-name ";
 
 //generates call and logs response for openbrewery
 $.ajax({
@@ -11,30 +10,4 @@ $.ajax({
     method: "GET"
 }).then(function(response) {
     console.log(response);
-});
-
-//Ticketmast call
-$.ajax({
-    type:"GET",
-    url:"https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=" + tmKey + "&city=" + city,
-    async:true,
-    dataType: "json",
-    success: function(json) {
-                console.log(json);
-                // Parse the response.
-                // Do other things.
-             },
-    error: function(err) {
-                // This time, we do not end up here!
-                console.log(err)
-             }
-  });
-
-// .on("click") function associated with Find Drinks button
-$("#find-drinks").on("click", function(event) {
-    event.preventDefault();
-    city = document.getElementById("city-field").value;
-
-    //logs updated city value to console
-    console.log(city);
 });
