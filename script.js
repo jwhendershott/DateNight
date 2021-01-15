13c42c575372dbcd55d541d91dd80dfd399509e4
Jacob Hendershott <jwhendershott@gmail.com>
2021-01-15 13:08:00 -0600

    added comments/removed rewritten script and unneeded variables

--- script.js
+++ script.js
@@ -2,33 +2,7 @@
 
 //set variables
 var city = document.getElementById("city-field");
-// const breweryURL = "https://api.openbrewerydb.org/breweries?by_city=" + city + "&sort=-name ";
 const tmKey = "8IbZBo40AnRzs987iA70DEBWvamJn6Ub"
-
-// //generates call and logs response for openbrewery
-// $.ajax({
-//     url: breweryURL,
-//     method: "GET"
-// }).then(function(response) {
-//     console.log(response);
-// });
-
-// //Ticketmast call
-// $.ajax({
-//     type:"GET",
-//     url:"https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=" + tmKey + "&city=" + city,
-//     async:true,
-//     dataType: "json",
-//     success: function(json) {
-//                 console.log(json);
-//                 // Parse the response.
-//                 // Do other things.
-//              },
-//     error: function(err) {
-//                 // This time, we do not end up here!
-//                 console.log(err)
-//              }
-//   });
 
 // .on("click") function associated with Find Drinks button
 $("#find-drinks").on("click", function(event) {
