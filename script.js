94be2a13072ccdcdcb983fc8b62267ab1f8c60e7
Rahul Chaphekar <rchaphekar1@gmail.com>
2021-01-18 23:28:58 -0600

    Mapbox API can call based on name of brewery

--- script.js
+++ script.js
@@ -29,11 +29,16 @@ $("#searchButton").on("click", function(event) {
         $("#phone").html("Phone Number: " + response[breweryChoice].phone);
         $("#street").html("Street: " + response[breweryChoice].street);
 
-        var streetAddress = response[breweryChoice].street + "%20";
-        var cityAddress = response[breweryChoice].city+ "%20";
-        var stateAddress = response[breweryChoice].state + "%20";
-        var zipCode = response[breweryChoice].postal_code + "%20";
-        mapboxAPI(streetAddress, cityAddress, stateAddress, zipCode);
+        // Creates variables for mapboxAPI and calls function
+        // var streetAddress = response[breweryChoice].street + "%20";
+        // var cityAddress = response[breweryChoice].city+ "%20";
+        // var stateAddress = response[breweryChoice].state + "%20";
+        // var zipCode = response[breweryChoice].postal_code + "%20";
+        // mapboxAPI(streetAddress, cityAddress, stateAddress, zipCode);
+        var breweryName = response[breweryChoice].name;
+        var longitude = response[breweryChoice].longitude;
+        var latitude = response[breweryChoice].latitude;
+        mapboxAPI(breweryName, longitude, latitude);
 
     });
     
@@ -73,15 +78,19 @@ $("#searchButton").on("click", function(event) {
 });
 
 // Function associated with Mapbox API
-function mapboxAPI(streetAddress, cityAddress, stateAddress, zipCode) {
-    console.log(streetAddress);
-    console.log(cityAddress);
-    console.log(stateAddress);
-    console.log(zipCode);
+function mapboxAPI(breweryName, longitude, latitude) {
+    // console.log(streetAddress);
+    // console.log(cityAddress);
+    // console.log(stateAddress);
+    // console.log(zipCode);
+    console.log(breweryName);
+    console.log(longitude);
+    console.log(latitude);
 
     // Generates and logs response from Mapbox
     $.ajax({
-        url: "https://api.mapbox.com/geocoding/v5/mapbox.places/" + streetAddress + cityAddress + stateAddress + zipCode + ".json?access_token=pk.eyJ1IjoicmNoYXBoZWthciIsImEiOiJja2swcHU5eGowZzlrMm9vdGZjYW41cmQyIn0.6LlorWQZFVTlXJcLezW6pw",
+        // url: "https://api.mapbox.com/geocoding/v5/mapbox.places/" + streetAddress + cityAddress + stateAddress + zipCode + ".json?access_token=pk.eyJ1IjoicmNoYXBoZWthciIsImEiOiJja2swcHU5eGowZzlrMm9vdGZjYW41cmQyIn0.6LlorWQZFVTlXJcLezW6pw",
+        url: "https://api.mapbox.com/geocoding/v5/mapbox.places/" + breweryName + ".json?proximity=" + longitude + "," + latitude + "&access_token=pk.eyJ1IjoicmNoYXBoZWthciIsImEiOiJja2swcHU5eGowZzlrMm9vdGZjYW41cmQyIn0.6LlorWQZFVTlXJcLezW6pw",
         method: "GET"
     }).then(function(response) {
         console.log(response);
